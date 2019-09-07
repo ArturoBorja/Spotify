df62b9bacbe18790967f43461f74746d1e669f57
ArturoBorja <53122722+ArturoBorja@users.noreply.github.com>
2019-09-07 11:33:18 -0500

    funciona un boton con timer

--- app/src/main/java/com/example/spotify/MainActivity.java
+++ app/src/main/java/com/example/spotify/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.spotify;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.drawable.RoundedBitmapDrawable;
 import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory;
 
+import android.annotation.SuppressLint;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
     ImageView img_song;
@@ -18,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     ImageView img_like;
     ImageView img_turn;
     ImageView img_bground;
+    ConstraintLayout lyt_main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
         img_like = findViewById(R.id.img_like);
         img_turn = findViewById(R.id.img_turn);
         img_play = findViewById(R.id.img_play);
+        lyt_main=findViewById(R.id.lyt_main);
 
         Drawable originalDrawable = img_song.getDrawable();
         Bitmap originalBitmap = ((BitmapDrawable) originalDrawable).getBitmap();
@@ -37,6 +44,38 @@ public class MainActivity extends AppCompatActivity {
         roundedDrawable.setCornerRadius(originalBitmap.getHeight());
         img_song.setImageDrawable(roundedDrawable);
 
+        img_bground.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("ResourceAsColor")
+            @Override
+            public void onClick(View view) {
+                //lyt_main.setBackgroundResource(R.color.playwhite);
+
+
+                CountDownTimer countDownTimer = new CountDownTimer(600, 30) {
+                    int a01 = 1;
+                    public void onTick(long millisUntilFinished) {
+                         if(a01 %2==0){
+                            img_bground.setBackgroundResource(R.color.bgrounddarck);
+                        }else{
+                            img_bground.setBackgroundResource(R.color.playwhite);
+                        }
+                        a01++;
+                    }
+
+                    public void onFinish() {
+                        img_bground.setBackgroundResource(R.color.itemnaorange);
+                    }
+                }.start();
+
+            }
+        });
+        img_bground.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                lyt_main.setBackgroundResource(R.color.itemnaorange);
+                return true;
+            }
+        });
 
     }
 }
